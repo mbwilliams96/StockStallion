21ced0ad83861453ea5783dc84d6f660324f9e17
Khalid <kalawady17@ufl.edu>
2018-03-23 15:24:08 -0400

    added a do-while loop in login prompt

--- StockStallion.cpp
+++ StockStallion.cpp
@@ -9,20 +9,24 @@ StockStallion::StockStallion() {
 }
 
 std::string StockStallion::loginRegisterPrompt() {
-    std::cout << "Welcome to StockStallion" << std::endl;
-    std::cout << "Please enter a selection: (1 or 2)" << std::endl;
-    std::cout << "[1] Register: \n";
-    std::cout << "[2] Log in " << std::endl;
     std::string choice;
-    std::cin >> choice;
+    do {
+        std::cout << "Welcome to StockStallion" << std::endl;
+        std::cout << "Please enter a selection: (1 or 2)" << std::endl;
+        std::cout << "[1] Register: \n";
+        std::cout << "[2] Log in " << std::endl;
+        std::cin >> choice;
+    }
+    while (choice != "1" || choice != "2");
     return choice;
 }
 
 bool StockStallion::verifyPassword(std::string pw) {
     try{
         std::ifstream infile;
-        infile.open("~/")
-    } catch (){
+        infile.open("~/Stockstallion.txt");
+
+    } catch (if){
 
     }
 }
