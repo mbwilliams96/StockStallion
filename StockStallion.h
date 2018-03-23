cf3dc101ece4d9be16b40990cb846eb8595c3441
Khalid <kalawady17@ufl.edu>
2018-03-23 15:02:01 -0400

    erased all methods and remade the login screen method verify is now type string

--- StockStallion.h
+++ StockStallion.h
@@ -1,16 +1,18 @@
 
 #ifndef STOCKSTALLION_H
 #define STOCKSTALLION_H
+
+#include <string>
 #include <limits>
 
 
 class StockStallion{
-	public:
-		StockStallion();
+public:
+	StockStallion();
 
-		// Database Functions
-		void initializeDB();
-		void addUserToDB(std::string username, std::string password);
+	// Database Functions
+	void initializeDB();
+	void addUserToDB(std::string username, std::string password);
 
     //templates suck for multi file projects : (
     template <typename T>
@@ -20,36 +22,36 @@ class StockStallion{
     bool verifyChoiceInRange(int choice, int max);
 
     // prompts user with options to login or register
-    void loginRegisterPrompt();
+    std::string loginRegisterPrompt();
 
 
-		bool verifyUsername(std::string username);
-		bool verifyPassword(std::string pw);
+	bool verifyUsername(std::string username);
+	bool verifyPassword(std::string pw);
 
     //gets user info (username, password), POSTS it to DB.
     void registerNewUser();
 
     void loginRegisterSequence();
-		void commandLineLoginRegisterView();
-		// bool authorizeLogin();
-		// std:: string getUsernameFromUser();
-		// std:: string getPasswordFromUser();
+	void commandLineLoginRegisterView();
+	// bool authorizeLogin();
+	// std:: string getUsernameFromUser();
+	// std:: string getPasswordFromUser();
 
 
-		// bool registerNewUser();
-		// void registrationSuccess();
-		// void registrationFailed();
+	// bool registerNewUser();
+	// void registrationSuccess();
+	// void registrationFailed();
 
-		// Main Menu which allows user to views stocks and perform other actions
-		// void commandLineMainMenuView();
-		// void displayOptions();
-		// int userChoice();
-		// void viewStocks();
+	// Main Menu which allows user to views stocks and perform other actions
+	// void commandLineMainMenuView();
+	// void displayOptions();
+	// int userChoice();
+	// void viewStocks();
 
 	// private:
-		// This is for a crude authentication system and must be changed.
-		// vector <User> userList;
-		// or another idea, read usernames and passwords from a file.
+	// This is for a crude authentication system and must be changed.
+	// vector <User> userList;
+	// or another idea, read usernames and passwords from a file.
 
 };
 #endif
