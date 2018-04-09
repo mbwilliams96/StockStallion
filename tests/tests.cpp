593bee654ff4ae231b69d01882404a14234d47ed
Tare <tare.dgg@gmail.com>
2018-04-09 19:21:50 -0400

    user verification from database can now be done.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,9 +2,10 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <mem.h>
 #include "../sqlite/sqlite3.h"
 #include "../StockStallion.h"
-
+#include "../Database.h"
 // template <typename T>
 // T requestInputFromUser(const std::string &userPrompt){
 //     std::cout << userPrompt << "\n> ";
@@ -19,30 +20,44 @@
 // }
 
 
-// Hello World
 
 
-
-
-int main(int argc, char* argv[]) {
-   sqlite3 *db;
-   char *zErrMsg = 0;
-   int rc;
-
-   rc = sqlite3_open("test.db", &db);
-2
-   if( rc ) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return(0);
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
-   }
-   sqlite3_close(db);
-}
-
-
-
-
+int main(){}
+//
+//    Database *db;
+//    db = new Database("stockstallion.db");
+//
+//
+//        //test
+//    std::string _id = std::to_string( rand() % 1000);
+//    std::string username = "example";
+//    std::string password = "exampled";
+//        // end test
+//
+//    //    char* q0 = "CREATE TABLE IF NOT EXISTS users( id INT PRIMARY KEY NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL);";
+////    std::string _q = std::string("INSERT INTO users(id, username,") + "password)" + "VALUES(" + "'" + _id + "'" + "," + "'" + username + "'" + "," + "'" + password + "'" + ");";
+////    char *q2 = &_q[0u];
+////    char* q2 = strdup(_q.c_str());
+//
+////    std::cout << q2;
+////    db->query(q2);
+////    //    db->query(q0);
+////    //    db->query("INSERT INTO a VALUES(1, 2);");
+////    //    db->query("INSERT INTO a VALUES(5, 4);");
+////    //    char* q = "SELECT name FROM sqlite_temp_master WHERE type='table';";
+//        char* q = "SELECT * FROM users;";
+//        vector<vector<string> > result = db->query(q);
+////
+//    //    std::cout << "\n";
+//        for(vector<vector<string> >::iterator it = result.begin(); it < result.end(); ++it)
+//        {
+//            vector<string> row = *it;
+//            cout << "ID: " << row.at(0) << "\t" << "Username: " << row.at(1) << "\t" << "Password: " << row.at(2) << endl;
+//        }
+//
+//        db->close();
+//    //    std::cout << "hello";
+//}
 
 
 
